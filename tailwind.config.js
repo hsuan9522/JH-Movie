c030b5f1fbcc03e9c5129209e703798a489a375f
Hsuan <jh558125@gmail.com>
2022-06-29 15:04:09 +0800

    Add configs

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    500: '#121319',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
