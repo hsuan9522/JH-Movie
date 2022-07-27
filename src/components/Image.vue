162ed69acb926e1b4ffc36466ee3b3d3cd1bd264
Hsuan <jh558125@gmail.com>
2022-07-27 16:34:45 +0800

    Update image component

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,19 +1,46 @@
+<template>
+    <img :src="image" :alt="alt" :class="css" />
+</template>
+
 <script setup>
 import { ref, watchEffect } from 'vue'
 
 const props = defineProps({
-  path: { type: String },
-  alt: { type: String },
-  css: { type: String },
+    path: { type: String },
+    alt: { type: String },
+    css: { type: String },
 })
 
+const fileType = props.path.match(/\.[0-9a-z]+$/i)
+const imageName = props.path.replace(fileType, '')
 const image = ref()
 
 watchEffect(async () => {
-  image.value = (await import(/* @vite-ignore */ `../assets/images/${props.path}`)).default
+    switch (fileType[0]) {
+        case '.jpg':
+            image.value = (
+                await import(
+                    /* @vite-ignore */ `../assets/images/${imageName}.jpg`
+                )
+            ).default
+        case '.png':
+            image.value = (
+                await import(
+                    /* @vite-ignore */ `../assets/images/${imageName}.png`
+                )
+            ).default
+            break
+        case '.svg':
+            image.value = (
+                await import(
+                    /* @vite-ignore */ `../assets/images/${imageName}.svg`
+                )
+            ).default
+            break
+        default:
+            console.log(
+                `Sorry, the image component can't recognize the ${fileType} file type just yet.`
+            )
+    }
 })
 </script>
-
-<template>
-  <img :src="image" :alt="alt" :class="css" />
-</template>
